b3a30c09dedb347145a2f07f044a8e38d6647e91
chmiftah <78901708+chmiftah@users.noreply.github.com>
2021-02-13 10:20:44 +0700

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -14,10 +14,8 @@ const routes = [
     path: '/surah/:id',
     name: 'surah',
     params: 'id',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Surah.vue')
+   
+    component: () => import('../views/Surah.vue')
   }
 ]
 
